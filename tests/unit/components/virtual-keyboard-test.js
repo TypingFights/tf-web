29920ef9c6918759a2bb68904c03435c220a0189
Daniil <filippovdaniil@gmail.com>
2017-12-31 16:17:18 +0700

    (+) Added another test for the virtual-keyboard component

--- tests/unit/components/virtual-keyboard-test.js
+++ tests/unit/components/virtual-keyboard-test.js
@@ -32,4 +32,44 @@ test('it should prompt a single key properly', function (expect) {
   component.set('error', false);
   expect(promptedState.Backspace).to.be.false;
   expect(promptedState.Numpad2).to.be.true;
+
+  expect(component.setProperties.bind(component, {
+    nextSymbolPosition: 2,
+    nextSymbol: '+',
+  })).to.not.throw();
+  expect(promptedState.Numpad1).to.be.false;
+  expect(promptedState.Numpad2).to.be.false;
+  expect(promptedState.Backspace).to.be.false;
+});
+
+test('it should prompt a key combination properly', function (expect) {
+  const physicalLayout = new PhysicalNumpadLayout();
+  const logicalLayout = new LogicalNumpadLayout();
+  physicalLayout.defaultCharCodesMap[35] = 'Shift Numpad3';
+  logicalLayout.charCodesMap[8470] = 35;
+  const component = this.subject({
+    layout: {
+      physical: physicalLayout,
+      logical: logicalLayout,
+    },
+  });
+  component.setProperties({
+    nextSymbolPosition: 0,
+    nextSymbol: '#',
+  });
+  const promptedState = component.get('promptedState');
+  expect(promptedState.Shift).to.be.true;
+  expect(promptedState.Numpad3).to.be.true;
+  component.setProperties({
+    nextSymbolPosition: 1,
+    nextSymbol: '1',
+  });
+  expect(promptedState.Shift).to.be.false;
+  expect(promptedState.Numpad3).to.be.false;
+  component.setProperties({
+    nextSymbolPosition: 2,
+    nextSymbol: '№',
+  });
+  expect(promptedState.Shift).to.be.true;
+  expect(promptedState.Numpad3).to.be.true;
 });
